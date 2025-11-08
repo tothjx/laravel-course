eb80a137c8b8badaeb9c7296f13705957748986f
Jozsef Toth <tothjx@gmail.com>
2025-11-08 12:35:45 +0100

    docker-hf-15

---
+++ docker/docker-hf-15/src/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>apache és php docker-el</title>
+    <style>
+        h1.stilusos-1 {
+            font-family: 'Arial Black', sans-serif, Helvetica;
+            font-size: 128pt;
+            font-weight: 900;
+            color: coral;
+            margin-block-start: 0;
+            margin-block-end: 0;
+            line-height: .75;
+        }
+        .stilusos-2 {
+            font-family: Arial, sans-serif, Helvetica;
+            font-size: 40pt;
+            font-weight: normal;
+            color: teal;
+            line-height: .1;
+        }
+        .stilusos-1,
+        .stilusos-2 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1 class="stilusos-1">hello</h1>
+        <p class="stilusos-2">
+            <b>php verzió:</b> <?php echo phpversion(); ?>
+        </p>
+    </div>
+</body>
+</html>
